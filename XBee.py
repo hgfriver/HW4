932a6a8272745dddc6c78058ba0176ed847edcea
hgfriver <hgfriver@gmail.com>
2020-06-12 21:48:32 +0800

    Add files via upload

---
+++ XBee.py
@@ -0,0 +1,158 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import paho.mqtt.client as paho
+import serial
+import time
+
+mqttc = paho.Client()
+
+'''
+s.write("+++".encode())
+
+char = s.read(2)
+
+print("Enter AT mode.")
+
+print(char.decode())
+
+
+s.write("ATMY <BASE_MY>\r\n".encode())
+
+char = s.read(3)
+
+print("Set MY <BASE_MY>.")
+
+print(char.decode())
+
+
+s.write("ATDL <BASE_DL>\r\n".encode())
+
+char = s.read(3)
+
+print("Set DL <BASE_DL>.")
+
+print(char.decode())
+
+
+s.write("ATID <PAN_ID>\r\n".encode())
+
+char = s.read(3)
+
+print("Set PAN ID <PAN_ID>.")
+
+print(char.decode())
+
+
+s.write("ATWR\r\n".encode())
+
+char = s.read(3)
+
+print("Write config.")
+
+print(char.decode())
+
+
+s.write("ATMY\r\n".encode())
+
+char = s.read(4)
+
+print("MY :")
+
+print(char.decode())
+
+
+s.write("ATDL\r\n".encode())
+
+char = s.read(4)
+
+print("DL : ")
+
+print(char.decode())
+
+
+s.write("ATCN\r\n".encode())
+
+char = s.read(3)
+
+print("Exit AT mode.")
+
+print(char.decode())
+
+'''
+
+# Settings for connection
+host = "localhost"
+topic= "Mbed"
+port = 1883
+
+# Callbacks
+def on_connect(self, mosq, obj, rc):
+    print("Connected rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed OK")
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+    print("Unsubscribed OK")
+
+# Set callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe    
+
+
+
+
+
+
+t = np.arange(0,20,1)
+x = np.arange(0,20,1)
+y = np.arange(0,20,1)
+z = np.arange(0,20,1)
+tilt = np.arange(0,20,1)
+
+# XBee setting
+
+serdev = '/dev/ttyUSB0'
+
+s = serial.Serial(serdev, baudrate = 9600)
+
+# Connect and subscribe
+print("Connecting to " + host + "/" + topic)
+mqttc.connect(host, port=1883, keepalive=60)
+mqttc.subscribe(topic, 0)
+
+for i in range(0, 25):
+    # send RPC to remote
+
+    s.write("/getAcc/run\r".encode())
+    time.sleep(1)
+    
+    if i > 5:
+        line=s.readline()
+        mqttc.publish(topic, line)
+        line=s.readline() 
+        x[i-5] = 1000*float(line)
+        line=s.readline()  
+        y[i-5] = 1000*float(line)
+        line=s.readline() 
+        z[i-5] = 1000*float(line)
+        line=s.readline() 
+        tilt[i-5] = float(line)
+        
+
+fig, ax = plt.subplots(2, 1)
+ax[0].stem(t,tilt)
+ax[0].set_xlabel('Time')
+ax[0].set_ylabel('Tilt')
+ax[1].plot(t,x/1000)
+ax[1].plot(t,y/1000)
+ax[1].plot(t,z/1000)
+ax[1].set_xlabel('Time')
+ax[1].set_ylabel('Acc Vector')
+plt.show()
+s.close()
---
+++ mqtt.py
@@ -0,0 +1,59 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import paho.mqtt.client as paho
+import time
+mqttc = paho.Client()
+
+
+host = "localhost"
+topic= "Mbed"
+port = 1883
+
+t = np.arange(0,18,1)
+r = np.arange(0,18,1)
+i = 0
+
+
+
+
+def on_connect(self, mosq, obj, rc):
+    print("Connected rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    global i
+    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n")
+    print(float(msg.payload))
+    print(i)
+    r[i] = float(msg.payload)
+    i += 1
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed OK")
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+    print("Unsubscribed OK")
+
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+
+
+print("Connecting to " + host + "/" + topic)
+mqttc.connect(host, port=1883, keepalive=60)
+mqttc.subscribe(topic, 0)
+
+while True:
+    mqttc.loop()
+    if i >= 18:
+        plt.figure()
+        plt.plot(t,r)
+        plt.xlabel('timestamp')
+        plt.ylabel('number')
+        plt.title("# collected data plot")
+        plt.show()
+        break
+
+#Loop forever, receiving messages
+#mqttc.loop_forever()
